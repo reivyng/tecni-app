93dfc13bca81bee444a23a8cad827a52216fc39e
JULY <danielitha345ra@gmail.com>
2025-03-17 06:50:19 -0500

    primera parte de la pagina de proyectos echa

--- tecni-app/src/pages/projects.jsx
+++ tecni-app/src/pages/projects.jsx
@@ -1,15 +1,40 @@
-import 'react';
+import "react";
+import { motion } from "framer-motion";
 
 const projects = () => {
-    return (
-        <div>
+  return (
+    <motion.div
+      className="p-6 m-6 mx-auto w-full max-w-6xl border-[1.5px] border-neutral-700 rounded-2xl opacity-0"
+      initial={{ opacity: 0, y: 50 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true }}
+      transition={{ duration: 0.8, ease: "easeOut" }}
+    >
+      {/* Contenedor principal */}
+      <div className="grid grid-cols-1 gap-6 text-center md:grid-cols-3 md:text-left">
+        {[
+          { value: "+200", text: "Proyectos completados" },
+          { value: "+3", text: "Años completados" },
+          { value: "+73%", text: "Clientes satisfechos" },
+        ].map((item, index) => (
+          <motion.div
+            key={index}
+            className="flex items-center justify-center space-x-4"
+            initial={{ opacity: 0, y: 30 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            viewport={{ once: true }}
+            transition={{ duration: 0.6, delay: index * 0.2, ease: "easeOut" }}
+          >
+            <div className="bg-[#96B2C7] rounded-full w-16 h-16 md:w-20 md:h-20"></div>
+            <div>
+              <h1 className="text-[#6380A6] text-xl md:text-2xl font-bold">{item.value}</h1>
+              <p className="text-sm md:text-base">{item.text}</p>
+            </div>
+          </motion.div>
+        ))}
+      </div>
+    </motion.div>
+  );
+};
 
-            <main>
-                <h1>Contactanos</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel libero at neque tincidunt aliquet. Donec vel purus vel ipsum sagittis fermentum. Nullam fermentum, ipsum ac condimentum pellentesque, justo velit ultrices neque, ac consectetur neque neque vel nunc. Integer vel ex sed nunc luctus varius. Nulla facilisi. Nullam euismod, mi vel commodo convallis, velit purus consectetur neque, ac ultricies sapien mi vel justo.</p>
-            </main>
-         
-        </div>
-    );
-}
 export default projects;
